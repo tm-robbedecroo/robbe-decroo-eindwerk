df350c8dfef0d9ffa5a6f1567fb580b0fa9f70cc
Robbe Decroo <r0840260@student.thomasmore.be>
2025-02-07 23:50:54 +0100

    fixed alignment on services banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -48,7 +48,7 @@ export default function LandingPage() {
           </div>
         </section>
         <section className="w-full py-12 md:py-24 lg:py-32 bg-gray-100 dark:bg-gray-800">
-          <div className="container px-4 md:px-6">
+          <div className="container px-4 mx-auto md:px-6">
             <div className="grid gap-10 sm:grid-cols-2 lg:grid-cols-3">
               <div className="flex flex-col items-center space-y-2 border-gray-800 p-4 rounded-lg">
                 <Calendar className="h-10 w-10 mb-2" />
