f5e860b99530e5364ea7c45edbf38f4f94eb959a
Robbe Decroo <r0840260@student.thomasmore.be>
2025-02-18 21:42:06 +0100

    fixed eslint build errors

--- src/db/actions.tsx
+++ src/db/actions.tsx
@@ -1,10 +1,8 @@
 "use server";
-import { signIn } from "@/lib/auth";
 import { db } from "./client";
 import { users } from "./schema";
 
 import { eq } from "drizzle-orm";
-import { redirect, RedirectType } from "next/navigation";
 
 export async function registerManager(formData: FormData) {
     try {
