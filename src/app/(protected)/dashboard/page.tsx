e8baa148e858eaf69cc84fbd71910f94429b8717
Robbe Decroo <r0840260@student.thomasmore.be>
2025-02-18 21:39:21 +0100

    added dashboard page

---
+++ src/app/(protected)/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import AppSidebar from "@/components/page-blocks/sidebar";
+import { getAuthUser } from "@/db/actions";
+import { auth } from "@/lib/auth";
+
+export default async function DashboardPage() {
+
+    const session = await auth();
+    const user = await getAuthUser(session?.user?.email as string);
+
+    return (
+        <AppSidebar user={user}/>
+    )
+}
